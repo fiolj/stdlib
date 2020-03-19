990a4219ea8725660da24ce3415970dc2e7ee09e
Juan Fiol <juanfiol@gmail.com>
2020-03-19 10:40:11 -0300

    Version added to gist

--- workbench/time_moments.f90
+++ workbench/time_moments.f90
@@ -2,67 +2,87 @@ program time_moments
   USE timing_moments, only: moment, mmoment
   implicit none
 
-  integer, parameter :: N = 200000000
-  real(8) :: x(N)
-  real(8) :: y1(N/5), y(N/5,5)
-  real(8) :: z
+  integer, parameter :: N = 100*1000*1000
+  integer, parameter :: Nloop=3
+
+  real(8) :: x(N), x1
+  real(8) :: y(N/5,5), z(N/1000,5,5,2,2)
+  real(8) :: y1(N/5), z1(N/1000,5,5,2)
   real(8) :: t1, t2
-  integer :: dim, order
+  real(8), dimension(Nloop) :: ts, tm
+  integer :: dim, order, Nmax
+  integer :: i
 
   order = 2
   call random_number(x)
 
 
   print '("One dimensional cases, length =", I12)', size(x)
+  print '("#  Size    tsum  tmean")'
+  Nmax = N/100
+  do i=1, Nloop
+    call cpu_time(t1)
+    x1 = moment(x(:Nmax), order, 1)
+    call cpu_time(t2)
+    ts(i) = t2-t1
+
+    call cpu_time(t1)
+    x1 = mmoment(x(:Nmax), order, 1)
+    call cpu_time(t2)
+    tm(i)=t2-t1
+    print "(I10, f6.3, f6.3)", Nmax, ts(i), tm(i)
+    Nmax = Nmax*10
+  end do
+
+  print *,''
+
+  print '("Two dimensional cases, shape =", I12,I2)', shape(y)
+
+  y = reshape(x,[N/5,5])
+  dim = 1
+
+  print '("Along the axis=",I1, " mean on ",I1, " arrays of dimension =", I8)', dim, size(y,2), size(y,dim)
 
   call cpu_time(t1)
-  z = moment(x, order, 1)
+  y1 = moment(y, order, dim)
   call cpu_time(t2)
   print '("Internal sum Time = ",f6.3," seconds.")', t2-t1
-
   call cpu_time(t1)
-  z = mmoment(x, order, 1)
+  y1 = mmoment(y, order, dim)
   call cpu_time(t2)
   print '("Call to mean Time = ",f6.3," seconds.")', t2-t1
 
+  dim = 2
+  print '("Along the axis=",I1, " mean on ",I8, " arrays of dimension =", I1)', dim, size(y,1), size(y,dim)
+  call cpu_time(t1)
+  y1 = moment(y, order, dim)
+  call cpu_time(t2)
 
+  print '("Internal sum Time = ",f6.3," seconds.")', t2-t1
+  call cpu_time(t1)
+  y1 = mmoment(y, order, dim)
+  call cpu_time(t2)
+  print '("Call to mean Time = ",f6.3," seconds.")', t2-t1
 
-  ! y = reshape(x,[N/5,5])
+  print *,''
 
+  print *,"Five dimensional cases, shape =", shape(y)
+  ! z = reshape(x, [N/1000,10,10,5,2])
+  call random_number(z)
 
-  ! print '("Two dimensional cases, shape =", I12,I2)', shape(y)
-  ! dim = 1
-  ! print '("Along the axis=",I1, " dimension=", I8)', dim, size(y,dim)
+  do dim = 1, 5
 
-  ! call cpu_time(t1)
-  ! y1 = moment_2_rdp_rdp(y, order, dim)
-  ! call cpu_time(t2)
-  ! ! print '("Internal sum =", g0)', y1(1:5)
-  ! print '("Internal sum Time = ",f6.3," seconds.")', t2-t1
-  ! ! print "(5(g0))", y1(5:10)
-  ! call cpu_time(t1)
-  ! y1 = moment_2m_rdp_rdp(y, order, dim)
-  ! call cpu_time(t2)
-  ! ! print '("Call to mean =", g0)', y1(1:5)
-  ! print '("Call to mean Time = ",f6.3," seconds.")', t2-t1
+    print '("Along the axis = ",I1)', dim
 
-  ! dim = 2
-  ! print '("Along the axis=",I1, " dimension=", I2)', dim, size(y,dim)
-
-  ! call cpu_time(t1)
-  ! y1 = moment_2_rdp_rdp(y, order, dim)
-  ! call cpu_time(t2)
-  ! ! print '("Internal sum =", g0)', y1(1:5)
-  ! print '("Internal sum Time = ",f6.3," seconds.")', t2-t1
-  ! ! print "(5(g0))", y1(5:10)
-  ! call cpu_time(t1)
-  ! y1 = moment_2m_rdp_rdp(y, order, dim)
-  ! call cpu_time(t2)
-  ! ! print '("Call to mean =", g0)', y1(1:5)
-  ! print '("Call to mean Time = ",f6.3," seconds.")', t2-t1
+    call cpu_time(t1)
+    z1 = moment(z, order, dim)
+    call cpu_time(t2)
+    print '("Internal sum Time = ",f6.3," seconds.")', t2-t1
+    call cpu_time(t1)
+    z1 = mmoment(z, order, dim)
+    call cpu_time(t2)
+    print '("Call to mean Time = ",f6.3," seconds.")', t2-t1
+  end do
 
 
 end program time_moments
-
-
-
